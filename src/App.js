bc74c0949eaa7eff46dbca7ff748b6a139ada2b8
Anders Nylund <anders.nylund@aalto.fi>
2018-10-02 21:22:49 +0300

    Parse csv

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Papa from 'papaparse';
 
 class App extends Component {
+
+  fileReader;
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: null,
+    };
+  }
+
+  handleFileLoaded = () => {
+    const content = this.fileReader.result;
+    const parsed = Papa.parse(content);
+    this.setState({
+      data: parsed
+    });
+  }
+
+  hanleFileChange = (e) => {
+    const file = e.target.files[0];
+    this.fileReader = new FileReader();
+    this.fileReader.onloadend = this.handleFileLoaded;
+    this.fileReader.readAsText(file);
+  }
+
+  // https://medium.com/@ilonacodes/front-end-shorts-how-to-read-content-from-the-file-input-in-react-17f49b293909
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {this.state.data ?
+        <div>
+          <h1>Done!</h1>
+          <div>
+            {this.state.data.data.map((row) => (<div>{row}</div>))}
+          </div>
+        </div> 
+        :
+          <div>
+            <h1>Upload yor CSV-file here</h1>
+            <input type='file' accept='.csv' onChange={this.hanleFileChange}></input>  
+          </div>
+        }
       </div>
     );
   }

08bd9307e64ed1653e0b8ab033ca1111ed3fc03a
Anders Nylund <anders.nylund@aalto.fi>
2018-10-03 09:47:12 +0300

    Add eslint and prettier

--- src/App.js
+++ src/App.js
@@ -1,73 +1,77 @@
 import React, { Component } from 'react';
 import Papa from 'papaparse';
-import { XYPlot, MarkSeries, HorizontalGridLines, VerticalGridLines, YAxis, XAxis } from 'react-vis';
+import {
+  XYPlot,
+  MarkSeries,
+  HorizontalGridLines,
+  VerticalGridLines,
+  YAxis,
+  XAxis
+} from 'react-vis';
 import moment from 'moment';
 import FileDrop from './components/FileDrop';
 
 class App extends Component {
-
-  fileReader;
-
   constructor(props) {
     super(props);
     this.state = {
-      data: null,
+      data: null
     };
   }
 
-  handleFileLoaded = () => {
-    const content = this.fileReader.result;
+  handleFileLoaded = fileReader => {
+    const content = fileReader.result;
     const parsed = Papa.parse(content, {
       header: true
     });
 
-    const manipulated = parsed.data.map(row => {
-      return {
-        x: moment(row['Pvm'], 'DD.MM.YYYY'),
-        y: parseInt(row['Saldo'].replace(',', '.')),
-      };
-    });
+    const manipulated = parsed.data.map(row => ({
+      x: moment(row.Pvm, 'DD.MM.YYYY'),
+      y: parseInt(row.Saldo.replace(',', '.'), 10)
+    }));
 
     console.log('manipulated', manipulated);
 
     this.setState({
       data: manipulated
     });
-  }
+  };
 
-  hanleFileChange = (e) => {
+  hanleFileChange = e => {
     const file = e.target.files[0];
-    this.fileReader = new FileReader();
-    this.fileReader.onloadend = this.handleFileLoaded;
-    this.fileReader.readAsText(file);
-  }
+    const fileReader = new FileReader();
+    fileReader.onloadend = () => this.handleFileLoaded(fileReader);
+    fileReader.readAsText(file);
+  };
 
   // https://medium.com/@ilonacodes/front-end-shorts-how-to-read-content-from-the-file-input-in-react-17f49b293909
 
   render() {
+    const { data } = this.state;
+
     return (
       <div className="App">
-        {this.state.data ?
-        <div>
-          <h1>Done!</h1>
+        {data ? (
           <div>
-          <XYPlot height={1200} width={1200}>
-            <HorizontalGridLines />
-            <VerticalGridLines />
-            <XAxis title="X Axis" position="start" />
-            <YAxis title="Y Axis" />
-            <MarkSeries data={this.state.data} />
-          </XYPlot>
+            <h1>Done!</h1>
+            <div>
+              <XYPlot height={1200} width={1200}>
+                <HorizontalGridLines />
+                <VerticalGridLines />
+                <XAxis title="X Axis" position="start" />
+                <YAxis title="Y Axis" />
+                <MarkSeries data={data} />
+              </XYPlot>
+            </div>
           </div>
-        </div> 
-        :
+        ) : (
           <div>
             <h1>Drag n Drop your CSV-file here</h1>
             <FileDrop />
             <p>or upload it</p>
-            <input type='file' accept='.csv' onChange={this.hanleFileChange}></input>  
+            <input type="file" accept=".csv" onChange={this.hanleFileChange} />
           </div>
-        }
+        )}
       </div>
     );
   }
--- src/components/FileDrop.js
+++ src/components/FileDrop.js
@@ -3,23 +3,19 @@ import styled from 'styled-components';
 
 const StyledDiv = styled.div`
   border: 5px solid lightblue;
-  width:  200px;
+  width: 200px;
   height: 100px;
 `;
 
-const handleDrop = (e) => {
+const handleDrop = e => {
   e.preventDefault();
   console.log(e.dataTransfer.files[0]);
 };
 
-const handleDragOver = (e) => {
+const handleDragOver = e => {
   e.preventDefault();
-}
+};
 
-const FileDrop = () => {
-  return (
-    <StyledDiv onDrop={handleDrop} onDragOver={handleDragOver} />
-  );
-}
+const FileDrop = () => <StyledDiv onDrop={handleDrop} onDragOver={handleDragOver} />;
 
 export default FileDrop;

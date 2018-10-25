12e27b1aa9f2a6371e9cf4a71f309d1131fb065d
Tran Tu <tu.tran@trimble.com>
2018-10-25 13:49:45 +0300

    Add localization for BottomNavigation

--- src/components/BottomNavigation.js
+++ src/components/BottomNavigation.js
@@ -8,6 +8,7 @@ import InfoIcon from '@material-ui/icons/Info';
 import InputIcon from '@material-ui/icons/Input';
 import CategoryIcon from '@material-ui/icons/Category';
 import ShowChartIcon from '@material-ui/icons/ShowChart';
+import { FormattedMessage } from 'react-intl';
 
 const Container = styled.div`
   position: fixed;
@@ -27,11 +28,11 @@ class SimpleBottomNavigation extends React.Component {
     return (
       <Container>
         <BottomNavigation value={activePageIndex} onChange={this.handleChange} showLabels>
-          <BottomNavigationAction label="Start" icon={<ArrowForward />} />
-          <BottomNavigationAction label="Info" icon={<InfoIcon />} />
-          <BottomNavigationAction label="Import" icon={<InputIcon />} />
-          <BottomNavigationAction label="Categorization" icon={<CategoryIcon />} />
-          <BottomNavigationAction label="Visualization" icon={<ShowChartIcon />} />
+          <BottomNavigationAction label={<FormattedMessage id="navigation.Start" />} icon={<ArrowForward />} />
+          <BottomNavigationAction label={<FormattedMessage id="navigation.Info" />} icon={<InfoIcon />} />
+          <BottomNavigationAction label={<FormattedMessage id="navigation.Import" />} icon={<InputIcon />} />
+          <BottomNavigationAction label={<FormattedMessage id="navigation.Categorization" />} icon={<CategoryIcon />} />
+          <BottomNavigationAction label={<FormattedMessage id="navigation.Visualization" />} icon={<ShowChartIcon />} />
         </BottomNavigation>
       </Container>
     );

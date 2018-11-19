474de457f634c4a79b621e079188b8669631a772
Ahmed Mansour <ahmed@mansour.fi>
2018-11-19 20:33:10 +0200

    add navigation buttons

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import Button from '@material-ui/core/Button';
 import LandingPage from './pages/LandingPage';
 import ImportPage from './pages/ImportPage';
 import InfoPage from './pages/InfoPage';
@@ -16,12 +17,29 @@ import NavigationAppBar, {
   VISUALIZE,
   EXPORT
 } from './components/NavigationAppBar';
+import { setPageIndex } from './reducers/appReducer';
 
 const Container = styled.div`
   max-width: 60rem;
   margin: 0 auto;
   padding: 2rem;
 `;
+
+const NavigationContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  max-width: 14rem;
+  padding: 2rem 0;
+  margin: 0 auto;
+`;
+
+// @TODO replace the styles object with a styled component.
+// for reference check: https://www.styled-components.com/docs/basics#adapting-based-on-props
+const getNavButtonStyle = isHidden => ({
+  visibility: isHidden ? 'hidden' : 'visible'
+});
 
 export class App extends Component {
   showPage = pageIndex => {
@@ -46,23 +64,63 @@ export class App extends Component {
     return <LandingPage />;
   };
 
+  handleBackClick = () => {
+    const { changePageIndex, activePageIndex } = this.props;
+    if (activePageIndex > LANDING) {
+      changePageIndex(activePageIndex - 1);
+    }
+  };
+
+  handleForwardClick = () => {
+    const { changePageIndex, activePageIndex } = this.props;
+    if (activePageIndex < EXPORT) {
+      changePageIndex(activePageIndex + 1);
+    }
+  };
+
   render() {
     const { activePageIndex } = this.props;
     return (
       <div>
         <NavigationAppBar />
-        <Container>{this.showPage(activePageIndex)}</Container>
+        <Container>
+          {this.showPage(activePageIndex)}
+          <NavigationContainer>
+            <Button
+              onClick={this.handleBackClick}
+              variant="contained"
+              component="span"
+              style={getNavButtonStyle(activePageIndex === LANDING)}
+            >
+              Back
+            </Button>
+            <Button
+              onClick={this.handleForwardClick}
+              variant="contained"
+              component="span"
+              style={getNavButtonStyle(activePageIndex === EXPORT)}
+            >
+              Next
+            </Button>
+          </NavigationContainer>
+        </Container>
       </div>
     );
   }
 }
 
 App.propTypes = {
-  activePageIndex: PropTypes.number.isRequired
+  activePageIndex: PropTypes.number.isRequired,
+  changePageIndex: PropTypes.func.isRequired
 };
 
 const mapStateToProps = state => ({
   activePageIndex: state.appReducer.activePageIndex
 });
 
-export default connect(mapStateToProps)(App);
+export default connect(
+  mapStateToProps,
+  {
+    changePageIndex: setPageIndex
+  }
+)(App);
